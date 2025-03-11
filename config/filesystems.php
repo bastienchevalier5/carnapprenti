0d6a59c4c583581ad1ad60ade5f1d8e36c61a6ab
Bastien <chevalierbastien770@gmail.com>
2025-03-11 19:09:56 +0100

    ajout backup

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,11 @@ return [
             'throw' => false,
         ],
 
+        'backups' => [
+        'driver' => 'local',
+        'root' => storage_path('app/backups'),
+        ],
+
     ],
 
     /*
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,6 +17,11 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
 
+            <div>
+                <a href="/">
+                    <img src="{{ asset('images/carnapprenti-removebg-preview.png') }}" alt="Mon Logo" class="w-50 mt-3">
+                </a>
+            </div>
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
